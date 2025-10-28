4dc93fd96bbddfbd78a5ffd919f22b23c1c6b9ec
Jerry Santiago <jerrysantiago128@gmail.com>
2025-10-28 12:10:23 -0400

    fix: updated rabbitMQ container hostname for script usage and connectivity; creating bash script to strat RMQ server via docker compose

--- infra/start-rmq-mgmnt.sh
+++ infra/start-rmq-mgmnt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [[ $(docker ps -a | grep rabbitMQ) ]]; then
-	echo "stopping the running instance of RabbitMQ..."
-	docker stop rabbitMQ && docker rm rabbitMQ	
-	echo "starting a clean instance of RabbitMQ..."
-	docker run --name rabbitMQ -p 5672:5672 -p 15672:15672 -d rabbitmq:4.2.0-management
+	echo "stopping the running instance of RabbitMQ-server..."
+	docker stop rabbitMQ-server && docker rm rabbitMQ-server	
+	echo "starting a clean instance of RabbitMQ--server..."
+	docker run --name rabbitMQ-server --hostname rabbitMQ-server -p 5672:5672 -p 15672:15672 -d rabbitmq:4.2.0-management
 else
-	echo "starting an instance of RabbitMQ..."
-	docker run --name rabbitMQ -p 5672:5672 -p 15672:15672 -d rabbitmq:4.2.0-management
+	echo "starting an instance of RabbitMQ-server..."
+	docker run --name rabbitMQ-server --hostname rabbitMQ-server -p 5672:5672 -p 15672:15672 -d rabbitmq:4.2.0-management
 fi
 
 echo "Access the Management Console by opening the browser and naivagting to: \nhttp://localhost:15672"
