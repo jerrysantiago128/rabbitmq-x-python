a0b1292b15187a3afa1ba7e9825dc69c697043a8
Jerry Santiago <jerrysantiago128@gmail.com>
2025-10-29 14:20:52 -0400

    feat: take converted python dictionary and validate 'sensor_id' if 'sensor_id' is valid, then determine the shape of the object based on the number of data point entries return the shape as a string and add it to the message dictionary to pass to the UI component will need to implement REST API/frontend component

--- clients/consumer/src/consume.py
+++ clients/consumer/src/consume.py
@@ -1,22 +1,61 @@
 from rabbitmq import RabbitMQ
 import json
 
-def callback(ch, method, properties, body):
-    print(f"Received {body}")
+
+
+def determine_shape(data_points):
+    num_data_points = len(data_points)
     
+    if num_data_points == 1:
+        shape = "point"
+    elif num_data_points == 2:
+        shape = "line"
+    elif num_data_points == 3:
+        shape = "triangle"
+    elif num_data_points == 4:
+        shape = "rectangle"
+    elif num_data_points == 5:
+        shape = "pentagon"
+    elif num_data_points == 6:
+        shape = "hexagon"
+    elif num_data_points == 7:
+        shape = "pentagon"
+    elif num_data_points == 8:
+        shape = "octagon"
+    elif num_data_points == 9:
+        shape = "heptagon"
+    elif num_data_points == 10:
+        shape = "decagon"
+    else:
+        raise ValueError(f"ERROR: The number of data points: {num_data_points} provided is not in  valid range. Must be between 1 and 10 points")
+    
+    return shape
+
+
+#def sendToUI(message):
+    # implement code to send message to REST API
+
+
+def callback(ch, method, properties, body):
+    #print(f"Received {body}")
     # convert from byte to string to python dictionary
     message = json.loads(body.decode('utf-8'))
-    onMessage(message)
+    process(message)
 
-def onMessage(message):
+
+def process(message):
     print(f"Processing new message...")
-    for key in message:
-        if message[key].find('SENSOR'):
-            # do something
-        #print(key, ":", message[key])
-        # print(type(message[key]))
-    # strip out content from json message
-
+    if "SENSOR" in message["sensor_id"]:
+        try:
+            shape = determine_shape(message["data_points"]) # input is type list; output is a string
+            # update message dictionary to pass to REST API
+            message["shape"] = shape
+            print(message)
+            #sendToUI(message)
+        except ValueError as error:
+            print(error)
+    else:
+        print(f"ERROR: \nSensor ID: {message['sensor_id']} not valid")
 
 
 queue_name = 'json_data'
