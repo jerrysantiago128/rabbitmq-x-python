47d033b3595ed38471c8e5ff8cf8778e2a451516
Jerry Santiago <jerrysantiago128@gmail.com>
2025-10-29 12:19:07 -0400

    feat: convert rabbitMQ message from type byte to python dict via json.loads(); begin basics of ETL data processing based on dictionary format

--- clients/consumer/src/consume.py
+++ clients/consumer/src/consume.py
@@ -1,16 +1,25 @@
 from rabbitmq import RabbitMQ
+import json
 
 def callback(ch, method, properties, body):
     print(f"Received {body}")
-    onMessage(body)
+    
+    # convert from byte to string to python dictionary
+    message = json.loads(body.decode('utf-8'))
+    onMessage(message)
 
 def onMessage(message):
     print(f"Processing new message...")
-    
+    for key in message:
+        if message[key].find('SENSOR'):
+            # do something
+        #print(key, ":", message[key])
+        # print(type(message[key]))
     # strip out content from json message
 
 
 
 queue_name = 'json_data'
 rabbitmq = RabbitMQ()
+print("Starting RabbitMQ Consumer")
 rabbitmq.consume(queue_name, callback)
